a01ad0169169331ca748c1e342b9c6a966314275
Ayomikun Ogunjuyigbe <ogunjuay@gmail.com>
2022-09-02 15:21:48 +0100

    updated settings.py for deployment

--- NumGen/NumGen/settings.py
+++ NumGen/NumGen/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import django_heroku
 import dj_database_url
 import os
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "$16i43l7ust%z(t5lyn&5(4u!d8uG19)el-z89gm0zgcmp6m^g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
